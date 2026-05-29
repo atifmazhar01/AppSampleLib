fe9eec0518d3e98d88dd3103db14b30f34ed9180
Atif Mazhar <atif.mazhar@amazehealth.com>
2026-05-29 18:20:19 +0500

    t2

---
+++ SampleLib/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    id("maven-publish") // 1. Apply the maven-publish plugin
+}
+
+android {
+    namespace = "com.example.samplelib"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 25
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+// 2. Configure the publishing details
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                // Coordinates used to consume the library
+                groupId = "com.github.atifmazhar01"
+                artifactId = "my-library-module"
+                version = "1.0.0"
+
+                // Applies the component created by the Android Gradle Plugin
+                from(components["release"])
+            }
+        }
+
+        // Define where the library will be built locally before pushing
+        repositories {
+            maven {
+                name = "LocalRepo"
+                url = uri(layout.buildDirectory.dir("outputs/maven-repo"))
+            }
+        }
+    }
+}
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
